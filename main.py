8fc39cf3427433fccd263bddf9d87a35f4ba04e2
subhamkg47 <subhamkg47@gmail.com>
2026-08-03 11:58:57 +0545

    Connect FastAPI with PostgreSQL

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from fastapi import FastAPI
+from sqlalchemy import text
+from database.database import engine
 
 app = FastAPI(
     title="Secure Login System API",
     version="1.0.0"
 )
 
+
 @app.get("/")
 def home():
-    return {
-        "message": "Welcome to Secure Login System API"
-    }
+    return {"message": "Secure Login System API is running!"}
+
+
+@app.get("/test-db")
+def test_database():
+    try:
+        with engine.connect() as connection:
+            connection.execute(text("SELECT 1"))
+        return {"message": "Database connected successfully!"}
+    except Exception as e:
+        return {"error": str(e)}
